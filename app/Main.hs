ad4ee3104f1af54ba00483df299e6817145d53a3
Sandeep Bhardwaj <bsandeep@thoughtworks.com>
2017-10-25 01:21:02 +0530

    Add parsing of git commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.Storage.Parser
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import Data.Compress.Compress
 
 main :: IO ()
 main = do
   path1 <- readFile ".git/HEAD"
   print path1
-  let (_: path2) = dropWhile (/=' ') path1
-  let (x : path) = reverse path2
+  let (_:path2) = dropWhile (/= ' ') path1
+  let (x:path) = reverse path2
   print ("the path here " ++ path)
   file <- readFile (".git/" ++ reverse path)
---  print gitObject content
-  print file
+  commit <- B.readFile ".git/objects/46/32c6921c87209d5f60b6e498be0dc1ea2a80f7"
+  let str = C.unpack $ decompress commit
+  let parsed = parseGitObject str
+  print parsed
--- src/Data/Storage/Parser.hs
+++ src/Data/Storage/Parser.hs
@@ -10,7 +10,7 @@ nul :: Parser ()
 nul = () <$ char '\0'
 
 ws :: Parser String
-ws = many $ char ' '
+ws = many $ oneOf " \n\t"
 
 gitType :: Parser ObjectType
 gitType =  GBlob   <$ string "blob"
@@ -36,3 +36,38 @@ entry = TreeEntry <$> (filetyp <* ws) <*> (gitType <* ws) <*> (sha1 <* ws) <*> f
 
 treeP :: Parser [TreeEntry]
 treeP = many1 entry
+
+
+commit :: Parser Commit
+commit = Commit <$> tre <*> parents <*> author <*> commiter <*> message
+  where
+    tre = string "tree " *> sha1 <* ws
+    parents = many parent
+    parent = string "parent " *> sha1 <* ws
+    author = string "author " *> many (noneOf "\n") <* ws
+    commiter = string "committer " *>  many (noneOf "\n") <* ws
+    message = many anyChar
+
+
+tag :: Parser Tag
+tag = Tag <$> obj <*> typ <*> name <*> owner <*> tagmsg
+  where
+    obj    = string "object " *> sha1
+    typ    = string "type "   *> many anyChar
+    name   = string "tag "    *> many anyChar
+    owner  = string "tagger " *> many anyChar
+    tagmsg = many anyChar
+
+
+gitObject :: Parser Object
+gitObject = do
+    (typ,len) <- header
+    case typ of
+        GBlob   -> GoBlob   len <$> blob len
+        GTree   -> GoTree   len <$> treeP
+        GCommit -> GoCommit len <$> commit
+        GTag    -> GoTag len <$> tag
+
+
+parseGitObject :: String -> Either ParseError Object
+parseGitObject text = parse gitObject "test" text
